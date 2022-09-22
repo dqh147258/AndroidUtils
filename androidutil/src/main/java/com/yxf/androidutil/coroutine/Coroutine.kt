f2d76938e0db440a1811af8df87b49153346725d
Carl <1083223454@qq.com>
2022-09-22 12:11:24 +0800

    add coroutine extension

---
+++ androidutil/src/main/java/com/yxf/androidutil/coroutine/Coroutine.kt
@@ -0,0 +1,47 @@
+package com.yxf.androidutil.coroutine
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+
+fun <T> Flow<T>.collectOnCoroutine(
+    coroutineScope: CoroutineScope,
+    block: (T) -> Unit,
+    coroutineContext: CoroutineContext
+) {
+    val flow = this
+    coroutineScope.launch(coroutineContext) {
+        flow.collect { block(it) }
+    }
+}
+
+fun <T> Flow<T>.collectOnCoroutine(coroutineScope: CoroutineScope, block: (T) -> Unit) {
+    collectOnCoroutine(coroutineScope, block, Dispatchers.Main)
+}
+
+fun <T> Flow<T>.catchOnCoroutine(
+    coroutineScope: CoroutineScope,
+    block: FlowCollector<T>.(Throwable) -> Unit,
+    coroutineContext: CoroutineContext
+): Flow<T> {
+    return catch {
+        val e = it
+        val fc = this
+        coroutineScope.launch(coroutineContext) {
+            block(e)
+        }
+    }
+}
+
+fun <T> Flow<T>.catchOnCoroutine(
+    coroutineScope: CoroutineScope,
+    block: FlowCollector<T>.(Throwable) -> Unit
+): Flow<T> {
+    return catchOnCoroutine(coroutineScope, block, Dispatchers.Main)
+}
